b940bff6e60480b0fd3625141e7f2bc654589d30
Davi Banfi <davibanfi1579@outlook.com>
2022-05-11 14:10:50 -0300

    feat: implement lifetime for buffers

--- src/http/method.rs
+++ src/http/method.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 pub enum Method {
     GET,
     POST,
@@ -8,4 +10,27 @@ pub enum Method {
     TRACE,
     PATCH,
     OPTIONS,
+}
+
+pub struct MethodError {
+
+}
+
+impl FromStr for Method {
+    type Err = MethodError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "GET" => Ok(Self::GET),
+            "POST" => Ok(Self::POST),
+            "PUT" => Ok(Self::PUT),
+            "DELETE" => Ok(Self::DELETE),
+            "HEAD" => Ok(Self::HEAD),
+            "CONNECT" => Ok(Self::CONNECT),
+            "TRACE" => Ok(Self::TRACE),
+            "PATCH" => Ok(Self::PATCH),
+            "OPTIONS" => Ok(Self::OPTIONS),
+            _ => Err(MethodError {}),
+        }
+    }
 }
