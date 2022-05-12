8254a922aed7f3dee807322dd8ae967eb520bc90
Davi Banfi <davibanfi1579@outlook.com>
2022-05-12 13:51:47 -0300

    feat: add file reader and html templates

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,14 @@ mod website_handler;
 
 use server::Server;
 use website_handler::WebsiteHandler;
+use std::env;
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
     let server = Server::new("127.0.0.1:3333".to_string());
-    server.run(WebsiteHandler);
+
+    println!("public path: {}", public_path);
+
+    server.run(WebsiteHandler::new(public_path));
 }
